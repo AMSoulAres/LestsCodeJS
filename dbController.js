d9f854e7ba6d115b8ca4e636ebad538aceb85755
F3540877 Gabriel Amorim Soares e Silva <amorimsoares@bb.com.br>
2023-01-24 15:32:30 -0300

    alterando cabecalho, adicionando grid para os produtos

--- dbController.js
+++ dbController.js
@@ -1,89 +1,5 @@
 class Db {
-  static baseUrl = "http://localhost:3000/posts";
   static productsUrl = "http://localhost:3000/profile"
-
-  static async addItem(item) {
-    try {
-      const res = await fetch(`${Db.baseUrl}`, {
-        method: "post",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(item),
-      });
-
-      const body = await res.json();
-
-      console.log(item);
-      return body;
-    } catch (error) {
-      console.log("Unexpected Error");
-      console.log(error);
-    }
-  }
-
-  static async update(id, item) {
-    const { _id, ...rest } = item;
-    try {
-      const res = await fetch(`${Db.baseUrl}/${id}`, {
-        method: "PUT",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(rest),
-      });
-
-      return await res.json();
-    } catch (err) {
-      console.log("Unexpected Error");
-      console.log(err);
-    }
-  }
-
-  static async remove(id) {
-    try {
-      const res = await fetch(`${Db.baseUrl}/${id}`, {
-        method: "DELETE",
-        headers: {
-          "content-type": "application/json",
-        },
-      });
-      return await res.json();
-    } catch (err) {
-      console.log("Unexpected Error remove");
-      console.log(err);
-    }
-  }
-
-  static async getOne(id) {
-    try {
-      const res = await fetch(`${Db.baseUrl}/${id}`, {
-        method: "GET",
-        headers: {
-          "content-type": "application/json",
-        },
-      });
-      return await res.json();
-    } catch (err) {
-      console.log("Unexpected Error");
-      console.log(err);
-    }
-  }
-  
-  static async getAll() {
-    try {
-      const res = await fetch(`${Db.baseUrl}`, {
-        method: "GET",
-        headers: {
-          "content-type": "application/json",
-        },
-      });
-      return await res.json();
-    } catch (err) {
-      console.log("Unexpected Error");
-      console.log(err);
-    }
-  }
 
   static async getOneProduct(id) {
     try {
@@ -93,7 +9,7 @@ class Db {
           "content-type": "application/json",
         },
       });
-      return await res.json();
+      return res.json();
     } catch (err) {
       console.log("Unexpected Error");
       console.log(err);
@@ -102,13 +18,13 @@ class Db {
   
   static async getAllProducts() {
     try {
-      const res = await fetch(`${Db.productsUrl}`, {
+      const res = await fetch(`${Db.productsUrl}/`, {
         method: "GET",
         headers: {
           "content-type": "application/json",
         },
       });
-      return await res.json();
+      return res.json();
     } catch (err) {
       console.log("Unexpected Error");
       console.log(err);
--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 import Db from "./dbController.js";
 
 function renderProducts(){
-    let section = document.querySelector(".productsList");
-    let product  = Db.getOneProduct(1);
-    console.log(product);
-}
+    let productsList = document.querySelector(".productsList-container");
+    Db.getAllProducts().then(products => {
+        console.log(products)
+        products.forEach(element => {
+        let cardHtml = document.createElement("div");
+        cardHtml.className = "card";
+        let imgHtml = document.createElement("img");
+        imgHtml.className = "imgCard";
+        let nameHtml  = document.createElement("h1");
+        nameHtml.className = "name"
+        let priceHtml = document.createElement("p");
+        priceHtml.className = "price";
+        let descHtml = document.createElement("desc");
+        descHtml.className = "desc";
+        let  addToCart = document.createElement("button")
+        addToCart.className = addToCart;
+        
+        cardHtml.appendChild(imgHtml);
+        cardHtml.appendChild(nameHtml);
+        cardHtml.appendChild(priceHtml);
+        cardHtml.appendChild(descHtml);
+        productsList.appendChild(cardHtml);
+                }
+            )
+        }
+    )
+};
 
 
 
 function add(product){
 }
 
-
-document.querySelector(".render").onclick = renderProducts
+window.addEventListener("load", renderProducts);
